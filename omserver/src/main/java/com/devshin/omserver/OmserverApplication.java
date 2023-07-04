2b1d92b8fa93847434725475bfe488da0ca8f26f
hosin86 <hosin86@gmail.com>
2023-07-04 14:07:28 +0900

    init

---
+++ omserver/src/main/java/com/devshin/omserver/OmserverApplication.java
@@ -0,0 +1,13 @@
+package com.devshin.omserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmserverApplication.class, args);
+	}
+
+}
---
+++ omserver/src/test/java/com/devshin/omserver/OmserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devshin.omserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
